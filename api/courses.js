164c8afcb465760820e2eef63dca646f7ac33e55
unknown <ragini.dindukurthi@gmail.com>
2023-06-04 18:48:16 -0700

    finished all endpoints except last 4 :(

--- api/courses.js
+++ api/courses.js
@@ -131,11 +131,131 @@ router.get('/:courseid', async function (req, res, next){
  * Route to update data for specific course, needs authentication.
  */
 router.put('/:courseid', async function (req, res, next){
-    if(ObjectId.isValid(courseid)){
-        
+
+    if(ObjectId.isValid(req.params.courseid)){
+        if(validateAgainstSchema(req.body, courseSchema)){
+            const db = getDb()
+            const collection = db.collection("courses")
+
+            const updateStatus = collection.replaceOne(
+                { _id: new ObjectId(req.params.courseid) },
+                req.body
+            )
+
+            if(updateStatus){
+                res.status(200).json({
+                    links: {
+                        course: `/courses/${req.params.courseid}`
+                    }
+                })
+            }
+            else{
+                next()
+            }
+        }
+        else{
+            res.status(400).json({error: "Request body is not a valid course object!"})
+        }
+    }
+    else{
+        next()
     }
 })
 
+/*
+ * Route to delete course from database, needs authentication. 
+ */
+router.delete("/:courseid", async function (req, res, next){
+
+    if(ObjectId.isValid(req.params.courseid)){
+        const db = getDb()
+        const collection = db.collection("courses")
+
+        const deleteStatus = await collection.deleteOne({_id: new ObjectId(req.params.courseid)})
+        if(deleteStatus){
+            res.status(204).end()
+        }
+        else{
+            next()
+        }
+    }
+    else{
+        next()
+    }
+})
+
+/*
+ * Route to get a list of students enrolled in a course, needs authentication. 
+ */
+
+router.get("/:courseid/students", async function (req, res, next){
+
+    if(ObjectId.isValid(req.params.courseid)){
+
+        res.status(200).json({students: "students"})
+    }
+    else{
+        next()
+    }
+})
+
+/*
+ * Route to update enrollment for a course, needs authentication.
+ */
+router.post("/:courseid/students", async function (req, res, next){
+
+    if(ObjectId.isValid(req.params.courseid)){
+        if((req.body.add && req.body.add.length > 0) || (req.body.remove && req.body.remove.length > 0)){
+            res.status(200).end()
+        }
+        else{
+            res.status(400).json({error: "Request body is not valid!"})
+        }
+        
+    }
+    else{
+        next()
+    }
+})
+
+/*
+ * Route to fetch a CSV file containing list of students enrolled in the course, needs authentication. 
+ */
+router.get("/:courseid/roster", async function (req, res, next){
+
+    if(ObjectId.isValid(req.params.courseid)){
+        //turn roster into a csv 
+
+        res.status(200).type("csv").send()
+    }
+    else{
+        next()
+    }
+})
+
+/*
+ * Route to fetch list of Assignments for the course.  
+ */
+router.get("/:courseid/assignments", async function (req, res, next){
+
+    if(ObjectId.isValid(req.params.courseid)){
+        const db = getDb()
+        const collection = db.collection("assignments")
+
+        const assignments = await collection.find({courseId: new ObjectId(req.params.courseid)})
+        if(assignments.length > 0){
+            res.status(200).json({
+                assignments: assignments
+            })
+        }
+        else{
+            next()
+        }
+    }
+    else{
+        next()
+    }
+})
 
 
 
