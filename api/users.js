4e3b55110ab287f8d5720fc02a430de2d0462bad
Andrea Tongsak <atongsak@gmail.com>
2023-06-10 13:36:24 -0700

    Some fixes for users

--- api/users.js
+++ api/users.js
@@ -1,9 +1,54 @@
-const { Router } = require('express')
-const { ValidationError } = require('sequelize')
-const bcrypt = require('bcryptjs')
+const router = require('express').Router()
 
-const { UserSchema } = require('../models/user')
-const { }
+const {
+    UserSchema,
+    insertNewUser,
+    getUserByEmail,
+    validateUser
+} = require('../models/user')
+const { generateAuthToken, requireAuthentication } = require("../lib/auth")
+
+// const {  } = require('../models/course')
+
+exports.router = router;
+
+router.post('/', async function (req, res) {
+    try {
+        const id = await insertNewUser(req.body);
+        console.log(id);
+        if (id) {
+            res.status(201).send({_id: id})
+        } else {
+            res.status(400).send({
+                error: "No."
+            })
+        }
+    } catch (err) {
+        console.log("Post Error: ", err)
+    }
+})
+
+router.get('/', async function (req.res) {
+    const db = getDb()
+    const collection = db.collection("users")
+    const usersPage = await collection.find({}).toArray()
+    res.status(200).json({usersPage})
+})
+
+router.post('/login', async function (req, res, next) {
+    if (req.body && req.body.email && req.body.password) {
+        try {
+            const authenticated = await validateUser(
+                req.body.email,
+                req.body.password
+            )
+            if (authenticated) {
+                const user = await getUserByEmail(req.body.email, true)
+                const token 
+            }
+        }
+    }
+})
 
 // admin
 
--- models/user.js
+++ models/user.js
@@ -1,34 +1,93 @@
 const { DataTypes } = require('sequelize')
 const { Course } = require('./course.js')
 
+const ObjectId = require('mongodb')
 const bcrypt = require('bcryptjs')
 
-const sequelize = require('../lib/sequelize')
+const { getDb } = require('../lib/mongo')
+const { extractValidFields } = require('../lib/validation.js')
 
 /**
  * Schema for a User
  */
-const User = sequelize.define('user', {
-    name: { type: DataTypes.STRING, allowNull: false },
-    name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, allowNull: false},
-    password: {
-        type: DataTypes.String,
-        set(value) (
-            this.setDataValue('password', bcrypt.hashSync(value));
-        ),
-        allowNull: false
-    },
-    role: { type: DataTypes.STRING, allowNull: false },
-    admin: { type: DataTypes.BOOLEAN, allowNull: true, defaultValue: false }
-})
+const UserSchema = {
+    id: { required: false },
+    name: { required: true },
+    email: { required: true },
+    password: { required: true },
+    role: { enum: ['admin', 'instructor', 'student'], default: 'student' },
+    admin: { required: false }
+}
+exports.UserSchema = UserSchema
 
-exports.User = User
+/*
+ * Insert a new User into the DB
+ */
+exports.insertNewUser = async function (user) {
+    try {
+        const userInsert = extractValidFields(user, UserSchema)
+        const hash = await bcrypt.hash(user.password, 8);
+        console.log(hash)
+        user.password = hash;
+        console.log(" -- user to insert: ", userInsert)
 
-exports.UserClientFields = [
-    'name',
-    'email',
-    'password',
-    'role',
-    'admin'
-]
+        const db = getDb()
+        const collection = db.collection('users')
+        const result = await collection.insertOne(userInsert)
+        return result.id
+    } catch (err) {
+        console.log("error in insertNewUser", err)
+        return null
+    }
+}
+
+/*
+ * Fetch a user from the DB based on user ID.
+ */
+async function getUserById (id, includePassword) {
+    const db = getDb()
+    const collection = db.collection('users')
+    if (!ObjectId.isValid(id)) {
+        return null
+    } else {
+        //project allows you to restrict fields (in this case, the password)
+        const results = await collection
+            .find({ _id: new ObjectId(id) })
+            .project(includePassword ? {} : { password: 0 })
+            .toArray()
+        console.log("found user: ", results[0])
+        return results[0]
+    }
+}
+
+exports.getUserById = getUserById
+
+/*
+ * Fetch a user from the DB based on user ID.
+ */
+async function getUserByEmail (email, includePassword) {
+    const db = getDb()
+    const collection = db.collection('users')
+    if (!ObjectId.isValid(email)) {
+        return null
+    } else {
+        //project allows you to restrict fields (in this case, the password)
+        const results = await collection
+            .find({ email: new ObjectId(email) })
+            .project(includePassword ? {} : { password: 0 })
+            .toArray()
+        console.log("found user: ", results[0])
+        return results[0]
+    }
+}
+
+exports.getUserByEmail = getUserByEmail
+
+exports.validateUser = async function (email, password) {
+    const user = await getUserById(email, true)
+    //once we know a user exists, compare plaintext and hashed + salted password 
+    //returns true -> if passwords match 
+    //returns false -> if passwords don't 
+    //bcrypt will hash the password and compare it with the stored password 
+    return user && await bcrypt.compare(password, user.password)  
+}
