824ca8e5660c77c0c2c681ded8b1041a023929b1
KakaposCanFly <73670799+KakaposCanFly@users.noreply.github.com>
2023-06-11 10:53:52 -0700

    submissions now upload to assignments objects. Added example submission data (#10)

--- api/assignments.js
+++ api/assignments.js
@@ -21,8 +21,8 @@ const crypto = require("node:crypto")
 exports.router = router;
 
 router.post('/', async function (req, res, next) {
-    console.log("body: ", req.body)
-    console.log("schema: ", AssignmentSchema)
+    // console.log("body: ", req.body)
+    // console.log("schema: ", AssignmentSchema)
     if (validateAgainstSchema(req.body, AssignmentSchema)) {
         try {
             const resultId = await insertNewAssignment(req.body)
@@ -144,17 +144,23 @@ router.post('/:id/submissions', upload.single("file"), async function (req, res,
             })
         }
         try {
-            const resultId = await insertNewSubmission(req.body)
+            const submissionId = await insertNewSubmission(req.body)
             const file = {
                 contentType: req.file.mimetype,
                 filename: req.file.filename,
                 path: req.file.path,
-                submissionId: req.body.submissionId
+                submissionId: new ObjectId(req.body.submissionId)
             }
             //Dev Note: May want to nest a try/catch block for this
             const fileId = await saveFile(file)
+
+            //Insert submissionId into submission list in assignment object
+            const db = getDb()
+            const collection = db.collection("assignments")
+            const updateStats = await collection.updateOne({ _id: new ObjectId(req.params.id)}, {$push: {submissions: submissionId}})
+
             res.status(201).json({
-                id: resultId,
+                id: submissionId,
             });
         } catch (err) {
             next(err)
@@ -172,7 +178,7 @@ router.get('/:id/submissions', async function (req, res, next) {
     const db = getDb()
     const collection = db.collection("submissions")
     try {
-        const submissions = await collection.find({ assignmentId: req.params.id }).toArray()        
+        const submissions = await collection.find({ assignmentId: new ObjectId(req.params.id) }).toArray()        
         if (submissions) {
             res.status(200).json(submissions)
         }
