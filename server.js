4206faa4dce0bf4de727cbc3963d99f20daa62d6
dodsonta <71948722+dodsonta@users.noreply.github.com>
2023-06-10 21:37:02 -0700

    Rate limiting basics done

--- server.js
+++ server.js
@@ -1,10 +1,59 @@
 const express = require('express');
+const redis = require('redis');
 
 const api = require('./api');
 const { connectToDb } = require("./lib/mongo")
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+const redisHost = process.env.REDIS_HOST || 'localhost'
+const redisPort = process.env.REDIS_PORT || 6379
+const redisClient = redis.createClient({
+    url: `redis://${redisHost}:${redisPort}`
+})
+
+const rateLimitWindowMillis = 60000
+const rateLimitMaxRequests = 10
+const rateLimitRefreshRate = rateLimitMaxRequests / rateLimitWindowMillis
+
+async function rateLimit(req, res, next) {
+    let tokenBucket
+    try {
+        tokenBucket = await redisClient.hGetAll(req.ip)
+    } catch (err) {
+        next()
+        return
+    }
+    tokenBucket = {
+        tokens: parseFloat(tokenBucket.tokens) || rateLimitMaxRequests,
+        last: parseFloat(tokenBucket.last) || Date.now()
+    }
+    const timeStamp = Date.now()
+    const ellapsedTime = timeStamp - tokenBucket.last
+    tokenBucket.tokens += ellapsedTime * rateLimitRefreshRate
+    tokenBucket.tokens = Math.min(tokenBucket.tokens, rateLimitMaxRequests)
+    tokenBucket.last = timeStamp
+
+    if (tokenBucket.tokens >= 1) {
+        tokenBucket.tokens -= 1
+        await redisClient.hSet(req.ip, [
+            ["tokens", tokenBucket.tokens],
+            ["last", tokenBucket.last]
+        ])
+        next()
+    } else {
+        await redisClient.hSet(req.ip, [
+            ["tokens", tokenBucket.tokens],
+            ["last", tokenBucket.last]
+        ])
+        res.status(429).json({
+            error: "Too many requests per minute"
+        })
+    }
+
+}
+app.use(rateLimit)
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -34,8 +83,10 @@ app.use('*', function (err, req, res, next) {
 })
 
 connectToDb(function () {
-  app.listen(port, function() {
-    console.log("== Server is running on port", port);
-  });
+    redisClient.connect().then(function (){
+        app.listen(port, function() {
+            console.log("== Server is running on port", port);
+         });
+    })
 })
 
